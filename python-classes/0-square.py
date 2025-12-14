5e1af1580dc4b060bb41231875ff49819ae2ca57
Firuza Isgandarli <firuze.isgenderli41@gmail.com>
2025-12-14 13:23:08 +0000

    Add 0-square task with main and README

---
+++ python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    This class defines a square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new Square instance.
+
+        Args:
+            size: size of the square
+        """
+        self.__size = size
---
+++ python-classes/README.md
@@ -0,0 +1,7 @@
+# Python Classes
+
+This directory contains tasks related to Python classes and objects.
+
+## 0. Square with size
+- Defines a Square class
+- Uses a private instance attribute `__size`
